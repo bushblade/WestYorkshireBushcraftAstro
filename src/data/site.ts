c80635d16b226d302aa40ace4854961b0482c05b
will <bushblade@gmail.com>
2026-08-09 17:50:37 +0100

    fix: update gallery links to goo.gl photos

--- src/data/site.ts
+++ src/data/site.ts
@@ -10,9 +10,9 @@ export const links = {
 	youtube: "https://www.youtube.com/channel/UCN563R-qyOcAKcLi8cH3bEA",
 	email: "mailto:westyorkshirebushcraftgroup@gmail.com",
 	galleries: {
-		campcraft: "https://bit.ly/2S8XB8h",
-		tracktrail: "https://bit.ly/2KC5A9C",
-		floraforage: "https://bit.ly/358Ihh5",
-		sitescenery: "https://bit.ly/3cPcGUd",
+		campcraft: "https://goo.gl/photos/wGSAgVFW6YDDPYYd7",
+		tracktrail: "https://goo.gl/photos/fNRV85FotBS543Qm8",
+		floraforage: "https://goo.gl/photos/wCNEi12U9jRWwuE7A",
+		sitescenery: "https://goo.gl/photos/vEZtXPgDppAqMWEf9",
 	},
 };
